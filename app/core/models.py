479f554a753fea31ccaa0d944ea5d2385242d814
Thamaraikannan28 <Thamaraikannan.cs19@bitsathy.ac.in>
2021-08-20 14:53:44 +0530

    Added ingredients and create ingredients feature

--- app/core/models.py
+++ app/core/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser,\
                                          BaseUserManager, PermissionsMixin
 from django.conf import settings
+# from django.db.models.deletion import CASCADE
 
 
 class UserManager(BaseUserManager):
@@ -38,6 +39,18 @@ class User(AbstractBaseUser, PermissionsMixin):
 
 class Tag(models.Model):
     """Tag to be used for a recipe"""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    """Ingredients to be used in a recipe"""
     name = models.CharField(max_length=255)
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -2,7 +2,7 @@
 # from django.db.models import fields
 from rest_framework import serializers
 
-from core.models import Tag
+from core.models import Tag, Ingredient
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -10,5 +10,14 @@ class TagSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Tag
-        fields = ['id', 'name']
+        fields = ('id', 'name')
+        read_only = ('id',)
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    """Serializers for Ingredient objects"""
+
+    class Meta:
+        model = Ingredient
+        fields = ('id', 'name')
         read_only = ('id',)
